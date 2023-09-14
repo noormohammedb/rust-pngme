1c91b84cda908b3eb07d947754ccc0232f1f3c39
Noor Mohammed B <noormohammedb@protonmail.com>
2023-09-14 10:33:56 +0400

    unit test for ChunkType

--- src/chunk_type.rs
+++ src/chunk_type.rs
@@ -1,1 +1,170 @@
+use std::{fmt::Display, str::FromStr};
 
+use crate::Error;
+
+#[derive(Debug)]
+struct ChunkType {
+  value: [u8; 4],
+}
+
+impl TryFrom<[u8; 4]> for ChunkType {
+  type Error = Error;
+  fn try_from(value: [u8; 4]) -> Result<Self, Self::Error> {
+    Ok(ChunkType {
+      // value: [1u8, 1u8, 1u8, 1u8],
+      value: unimplemented!(),
+    })
+  }
+}
+
+impl FromStr for ChunkType {
+  type Err = crate::Error;
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    Ok(ChunkType {
+      // value: [1u8, 1u8, 1u8, 1u8],
+      value: unimplemented!(),
+    })
+  }
+}
+
+impl Display for ChunkType {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    // Ok(())
+    unimplemented!()
+  }
+}
+
+impl PartialEq for ChunkType {
+  fn eq(&self, other: &Self) -> bool {
+    unimplemented!()
+  }
+}
+
+impl Eq for ChunkType {
+  // fn assert_receiver_is_total_eq(&self) {
+  //   unimplemented!()
+  // }
+}
+
+impl ChunkType {
+  fn bytes(&self) -> [u8; 4] {
+    unimplemented!()
+  }
+
+  fn is_valid(&self) -> bool {
+    unimplemented!()
+  }
+
+  fn is_critical(&self) -> bool {
+    unimplemented!()
+  }
+
+  fn is_public(&self) -> bool {
+    unimplemented!()
+  }
+
+  fn is_reserved_bit_valid(&self) -> bool {
+    unimplemented!()
+  }
+
+  fn is_safe_to_copy(&self) -> bool {
+    unimplemented!()
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  pub fn test_chunk_type_from_butes() {
+    let expected = [82, 117, 83, 116];
+    let actual = ChunkType::try_from([82, 117, 83, 116]).unwrap();
+
+    assert_eq!(expected, actual.bytes())
+  }
+
+  #[test]
+  pub fn test_chunk_type_from_str() {
+    let expected = ChunkType::try_from([82, 117, 83, 116]).unwrap();
+    let actual = ChunkType::from_str("RuSt").unwrap();
+
+    assert_eq!(expected, actual);
+  }
+
+  #[test]
+  pub fn test_chunk_type_is_critical() {
+    let chunk = ChunkType::from_str("RuSt").unwrap();
+    assert!(chunk.is_critical())
+  }
+
+  #[test]
+  pub fn test_chunk_type_is_not_critical() {
+    let chunk = ChunkType::from_str("ruSt").unwrap();
+    assert!(!chunk.is_critical());
+  }
+
+  #[test]
+  pub fn test_chunk_type_is_public() {
+    let chunk = ChunkType::from_str("RUSt").unwrap();
+    assert!(chunk.is_public());
+  }
+
+  #[test]
+  pub fn test_chunk_type_is_not_public() {
+    let chunk = ChunkType::from_str("RuSt").unwrap();
+    assert!(!chunk.is_public());
+  }
+
+  #[test]
+  pub fn test_chunk_type_is_reserved_bit_valid() {
+    let chunk = ChunkType::from_str("RuSt").unwrap();
+    assert!(chunk.is_reserved_bit_valid());
+  }
+
+  #[test]
+  pub fn test_chunk_type_is_reserved_bit_invalid() {
+    let chunk = ChunkType::from_str("Rust").unwrap();
+    assert!(!chunk.is_reserved_bit_valid());
+  }
+
+  #[test]
+  pub fn test_chunk_type_is_safe_to_copy() {
+    let chunk = ChunkType::from_str("RuSt").unwrap();
+    assert!(chunk.is_safe_to_copy());
+  }
+
+  #[test]
+  pub fn test_chunk_type_is_unsafe_to_copy() {
+    let chunk = ChunkType::from_str("RuST").unwrap();
+    assert!(!chunk.is_safe_to_copy());
+  }
+
+  #[test]
+  pub fn test_valid_chunk_is_valid() {
+    let chunk = ChunkType::from_str("RuSt").unwrap();
+    assert!(chunk.is_valid());
+  }
+  #[test]
+  pub fn test_invalid_chunk_is_valid() {
+    let chunk = ChunkType::from_str("Rust").unwrap();
+    assert!(!chunk.is_valid());
+
+    let chunk = ChunkType::from_str("Rult");
+    assert!(chunk.is_err());
+  }
+  #[test]
+  pub fn test_chunk_type_string() {
+    let chunk = ChunkType::from_str("RuSt").unwrap();
+    assert_eq!(&chunk.to_string(), "RuSt");
+  }
+
+  #[test]
+  pub fn test_chunk_type_trait_impls() {
+    let chunk_type_1: ChunkType = TryFrom::try_from([82, 117, 83, 116]).unwrap();
+    let chunk_type_2: ChunkType = FromStr::from_str("RuSt").unwrap();
+
+    let _chunk_string = format!("{}", chunk_type_1);
+    let _are_chunks_equal = chunk_type_1 == chunk_type_2;
+  }
+}
